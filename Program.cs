9ac3158cc838782dd25a7b44564db06f0cc5cec9
Matt Nev <mattnev@microsoft.com>
2021-08-21 16:06:28 -0500

    Push initial project to GitHub

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number");
+            var num = Console.ReadLine();
+            var number = Convert.ToInt32(num);
+        }
+    }
+}
