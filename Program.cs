3d816ab2f4a823fd16a8ec964c41f25c7d49e16d
Matt Nev <mattnev@microsoft.com>
2021-08-21 20:16:26 -0500

    Add exception handling to first input

--- Program.cs
+++ Program.cs
@@ -8,6 +8,20 @@ namespace Calculator
         {
             int number1 = 0; int number2 = 0;
 
+            try
+            {
+                Console.WriteLine("Enter a number");
+                number1 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("An error occured.");
+            }
+            finally
+            {
+                Console.WriteLine("Please try again.");
+            }
+
             Console.WriteLine("Enter a number");
             number1 = Convert.ToInt32(Console.ReadLine());
 
@@ -36,7 +50,6 @@ namespace Calculator
                     Console.WriteLine($"Result: {number1} / {number2} = " + (number1 / number2));
                     break;
             }
-
         }
     }
 }
